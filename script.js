a35f5a8c4c1a2eeec8c463e5c2bcebbf7d4a18a5
BEKSULTAN <230599@astanait.edu.kz>
2024-11-15 16:23:04 +0500

    demo TD template

---
+++ script.js
@@ -0,0 +1,6 @@
+// // script.js: Rotating the card on click
+// const card = document.querySelector('.card');
+
+// card.addEventListener('click', () => {
+//   card.classList.toggle('flipped'); // Toggle the 'flipped' class to rotate
+// });
